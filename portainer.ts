61798d0334ed163456f8c7b478e62a19be3b8ef8
Erick Tran <thelittlebotengineer@gmail.com>
2025-12-20 00:01:50 -0800

    feat(portainer.ts): start main class

--- portainer.ts
+++ portainer.ts
@@ -1,15 +1,30 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import https from 'https';
-import { config } from 'dotenv';
+import dotenv from 'dotenv';
 
 if (!process.env.PORTAINER_URL) {
+    // Suppress console output during dotenv configuration
     const originalConsoleLog = console.log;
     const originalConsoleInfo = console.info;
     console.log = () => {};
     console.info = () => {};
     
-    config({ path: '.env', debug: false });
+    dotenv.config({ path: '.env', debug: false });
     
+    // Restore original console functions
     console.log = originalConsoleLog;
     console.info = originalConsoleInfo;
+}
+
+export class PortainerApiClient {
+    private portainerUrl: string; // Portainer URL, must be defined
+    private accessToken: string | undefined; // Access token, must be defined for API calls
+
+    /**
+     */
+    constructor(
+        portainerUrl: string = process.env.PORTAINER_URL || 'http://localhost:9000',
+    ) {
+        this.portainerUrl = portainerUrl;
+    }
 }
